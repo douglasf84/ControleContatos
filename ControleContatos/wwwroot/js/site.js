058a2bcb58daf050b0c785170c801f34abe45a00
Douglas R. Ferreira <douglas.sp.br@hotmail.com>
2022-12-27 11:08:31 -0300

    alteração do modal

--- ControleContatos/wwwroot/js/site.js
+++ ControleContatos/wwwroot/js/site.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     getDatatable('#table-contatos');
     getDatatable('#table-usuarios');
 
-    $('.btn-total-contatos').click(function () { // usa . quando é classe e usa # quando é id
+    $('.btn-total-contatos').click(function () {
         var usuarioId = $(this).attr('usuario-id');
 
         $.ajax({
@@ -15,11 +15,11 @@ $(document).ready(function () {
             url: '/Usuario/ListarContatosPorUsuarioId/' + usuarioId,
             success: function (result) {
                 $("#listaContatosUsuario").html(result);
+                //$('#modalContatosUsuario1').modal().show();
                 getDatatable('#table-contatos-usuario');
-                $('#modalContatosUsuario').modal().show();
             }
         });
-    });  
+    });
 })
 
 function getDatatable(id) {
@@ -29,8 +29,8 @@ function getDatatable(id) {
         "searching": true,
         "oLanguage": {
             "sEmptyTable": "Nenhum registro encontrado na tabela",
-            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfo": "Mostrar _START_ at&eacute; _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 at&eacute; 0 de 0 Registros",
             "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
             "sInfoPostFix": "",
             "sInfoThousands": ".",
@@ -53,8 +53,13 @@ function getDatatable(id) {
     });
 }
 
+
 $('.close-alert').click(function () {
-    $('.alert').hide('hide');
+    $(".alert").hide('hide');
+});
+
+$('.btn-close-modal').click(function () {
+    $(".modalContatosUsuario").hidden('hidden');
 });
 
 $(document).ready(function () {
